dc5e91a5c4f63d3862a2ba666e6a5ff984e09bbe
hangsunlee <hslee22@sen.go.kr>
2016-11-02 15:27:38 +0900

    파일업로드 예제

---
+++ exupload.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="fupload.php" method="POST" enctype="multipart/form-data">
+          <label for="fupload">첨부파일
+          <input id="fupload" type="file" name='upload'>
+          <input type="submit" value="올리기">
+        </form>
+        
+        <?php
+        // put your code here
+        
+        
+        ?>
+    </body>
+</html>
